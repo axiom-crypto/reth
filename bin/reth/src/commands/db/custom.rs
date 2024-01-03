f8a6f1fe2901c1c98832c8bfb6ea282c8dc423b5
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2024-01-02 21:58:32 -0500

    feat: go through whole AccountsTrie table

--- bin/reth/src/commands/db/custom.rs
+++ bin/reth/src/commands/db/custom.rs
@@ -35,30 +35,45 @@ pub fn get_state_trie_depth<'a>(tool: DbTool<'a, DatabaseEnv>) -> eyre::Result<(
         println!("Total AccountsTrie entries: {}", total_entries);
         let page_size = stats.page_size() as usize;
         println!("Page size: {}", page_size);
-        let filter = ListFilter {
-            skip: 0,
-            len: page_size,
-            search: vec![],
-            min_row_size: 0,
-            min_key_size: 0,
-            min_value_size: 0,
-            reverse: false,
-            only_count: false,
-        };
-        let max_depth_in_page = retry_until_success(|| {
-            let mut max_depth = 0;
-            let (entries, _hits) = tool.list::<AccountsTrie>(&filter)?;
-            for (stored_nibbles, _stored_branch_node) in entries {
-                println!(
-                    "Stored nibbles: {:?}, branch node: {:?}",
-                    stored_nibbles, _stored_branch_node
-                );
-                let num_nibbles = stored_nibbles.0.len();
-                max_depth = max_depth.max(num_nibbles);
-            }
-            Ok(max_depth)
-        });
-        println!("Max depth in page: {}", max_depth_in_page);
+        let mut start = 0;
+        let mut global_max_depth = 0;
+        while start < total_entries {
+            let filter = ListFilter {
+                skip: start,
+                len: page_size,
+                search: vec![],
+                min_row_size: 0,
+                min_key_size: 0,
+                min_value_size: 0,
+                reverse: false,
+                only_count: false,
+            };
+            let max_depth_in_page = retry_until_success(|| {
+                let mut max_depth = 0;
+                let (entries, _hits) = tool.list::<AccountsTrie>(&filter)?;
+                for (stored_nibbles, _stored_branch_node) in entries {
+                    let mut mask_bits = 0;
+                    for i in 0..16 {
+                        if _stored_branch_node.0.state_mask.is_bit_set(i) {
+                            mask_bits += 1;
+                        }
+                    }
+                    if mask_bits <= 1 {
+                        println!(
+                            "Stored nibbles: {:?}, branch node: {:?}",
+                            stored_nibbles, _stored_branch_node
+                        );
+                    }
+                    let num_nibbles = stored_nibbles.0.len();
+                    max_depth = max_depth.max(num_nibbles);
+                }
+                Ok(max_depth)
+            });
+            println!("Max depth in page: {}", max_depth_in_page);
+            global_max_depth = global_max_depth.max(max_depth_in_page);
+            start += page_size;
+        }
+        println!("Global AccountTrie max depth: {}", global_max_depth);
     })?;
     Ok(())
 }
